123e8a67c69f649260fbb7d04f3298ca5bb59d62
uzumaki.apar <apu8467@gmail.com>
2023-07-22 17:08:04 -0500

    rest countries api

---
+++ part2/countries/src/services/dataService.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+const baseUrl = 'https://restcountries.com/v3.1/all'
+
+const getAll = () => {
+  const request = axios.get(baseUrl)
+  return request.then(response => response.data)
+}
+
+
+
+const dataService = {getAll}
+
+export default dataService
